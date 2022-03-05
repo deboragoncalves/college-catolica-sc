e7fa0bd6ff9430a7e2b2444265a488cbede82ee8
Débora Gonçalves <deboracristinapereira123@gmail.com>
2022-03-05 19:20:56 -0300

    Exercícios Vetores e Matrizes

---
+++ first-semester/estrutura-dados/vetores-matrizes/exercise-1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+
+                Exercício 1: Criar um vetor com 5 posições e ler os valores. 
+                Criar duas funções, que irão receber o vetor como parâmetro, 
+                uma função irá calcular a soma dos números pares do vetor 
+                e a outra retorna a quantidade de números ímpares.
+            
+            */
+            
+            Random number = new Random();
+
+            int[] notes = new int[5];
+
+            for (int a = 0; a < notes.Length; a++)
+            {
+                // aleatório entre 0 e 10
+                notes[a] = number.Next(1, 11);
+            }
+
+            Console.Write("Vetor: ");
+            Console.WriteLine(String.Join(", ", notes));
+            sumPairNumbers(notes);
+            totalOddNumbers(notes);
+        }
+
+        private static int sumPairNumbers(int[] notes) {
+
+            int sumPair = 0;
+
+            for (int i = 0; i < notes.Length; i++) {
+                if (notes[i] % 2 == 0) {
+                    sumPair += notes[i];
+                }
+            }
+
+            Console.WriteLine("Soma dos números pares: " + sumPair);
+            return sumPair;
+        }
+        private static int totalOddNumbers(int[] notes) {
+
+            int totalOdd = 0;
+
+            for (int i = 0; i < notes.Length; i++) {
+                if (notes[i] % 2 != 0) {
+                    totalOdd++;
+                }
+            }
+
+            Console.WriteLine("Quantidade de números ímpares: " + totalOdd);
+            return totalOdd;
+        }        
+    }
+}
